92bfa0f024d4f01386fc89ab1d9fc29e84d90a2d
DESKTOP-DML7QNJ\qtt <ware4u@gmail.com>
2024-06-12 16:06:53 +0700

    update submenu

--- src/js/menu.js
+++ src/js/menu.js
@@ -24,20 +24,15 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// Menu mobile
-// document.addEventListener('DOMContentLoaded', function () {
-//     const menuBackground = document.querySelector('.menu-mobile-background');
-//     const menuLinks = document.querySelectorAll('.nav__link-mobile');
+document.addEventListener('DOMContentLoaded', () => {
+    const hasSubmenu = document.querySelector('.has_submenu');
+    const submenu = hasSubmenu.querySelector('.submenu');
+    const icon = hasSubmenu.querySelector('.ri-add-line');
 
-//     menuLinks.forEach(link => {
-//         link.addEventListener('mouseenter', function () {
-//             const rect = this.getBoundingClientRect();
-//             const containerRect = this.parentNode.parentNode.getBoundingClientRect();
-
-//             menuBackground.style.width = `${rect.width}px`;
-//             menuBackground.style.height = `${rect.height}px`;
-//             menuBackground.style.left = `${rect.left - containerRect.left}px`;
-//             menuBackground.style.top = `${rect.top - containerRect.top}px`;
-//         });
-//     });
-// });
+    hasSubmenu.addEventListener('click', (event) => {
+        event.preventDefault();
+        submenu.classList.toggle('hidden');
+        submenu.classList.toggle('active');
+        icon.classList.toggle('rotate-90');
+    });
+});
