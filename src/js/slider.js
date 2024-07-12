977259f467042eff4b4263b3a8eb1abf55539e4f
DESKTOP-DML7QNJ\qtt <ware4u@gmail.com>
2024-07-12 18:48:11 +0700

    update swiperjs truong doi tac

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,21 +1,19 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 4,
-    spaceBetween: 32,
+    slidesPerView:4,    
+    spaceBetween:48,
     loop: true,
-
-    // If we need pagination
-    // pagination: {
-    //     el: '.swiper-pagination',
-    // },
-
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    watchOverflow: true,
     // Navigation arrows
     navigation: {
         nextEl: '.custom-swiper-button-next',
         prevEl: '.custom-swiper-button-prev',
+        clickable: true,
+        hideOnClick: false
     },
-
     breakpoints: {
         // when window width is >= 320px
         320: {
@@ -29,18 +27,59 @@ const swiper = new Swiper('.swiper', {
         },
         // when window width is >= 640px
         640: {
-            slidesPerView:3,
+            slidesPerView:2,
             spaceBetween: 30
         },
+
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+
         1024: {
             slidesPerView: 4,
             spaceBetween: 30
         }
 
-    }
-
-    // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
+    }    
 });
+
+
+const swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper_btn_next',
+        prevEl: '.swiper_btn_prev'
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+         
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView:2,
+            spaceBetween: 30
+        },
+
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+
+    }   
+});
+
